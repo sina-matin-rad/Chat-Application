a1e9e0525ad26a4edda58d398f493727aed8ff7e
sina-matin-rad <122496889+sina-matin-rad@users.noreply.github.com>
2024-08-07 12:10:47 +0330

    Define variables

---
+++ socket.io/socket.io.js
@@ -0,0 +1,9 @@
+// server.js
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
