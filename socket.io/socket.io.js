ad6e4f9b3a591e53ea701db49b4117d8ad1f348c
sina-matin-rad <122496889+sina-matin-rad@users.noreply.github.com>
2024-08-07 12:23:57 +0330

    Creating a Node.js server using Socket.IO

--- socket.io/socket.io.js
+++ socket.io/socket.io.js
@@ -7,3 +7,21 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('sendMessage', (message) => {
+        io.emit('receiveMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
